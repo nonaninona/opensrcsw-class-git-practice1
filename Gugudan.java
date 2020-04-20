22e5b79b7ca6045a77894170dd1c0d367ac0af8c
nonaninona <starcraft0529@gmail.com>
2020-04-20 14:48:32 +0900

    gugudan v1.5

--- Gugudan.java
+++ Gugudan.java
@@ -7,7 +7,16 @@ public class Gugudan {
 		
 		Scanner in = new Scanner(System.in);
 		int input = in.nextInt();
+
 		
+		if(input == 0) {
+			for(int i = 2;i<=9;i++) {
+				for(int j = 1;j<=9;j++) {
+					System.out.println(i + " X "  + j + " = " + i*j);
+				}
+			}
+			return;
+		}
 		for(int i=1;i<=9;i++) {
 			System.out.println(input + " X " + i + " = " + input*i);
 		}
